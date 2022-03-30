c5004e1cfc8d0d3524b144437422a4c74ffed35f
Jacob Immich <jacobimmich@gmail.com>
2022-03-30 17:30:34 -0400

    Add solution for problem #020

---
+++ 020/020.cpp
@@ -0,0 +1,34 @@
+// Problem 20 -- Factorial digit sum
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void multiply(vector<unsigned int>& v, unsigned int n)
+{
+    unsigned int p = 0;
+    unsigned int c = 0;
+    for (auto rit = v.rbegin(); rit != v.rend(); rit++)
+    {
+        p = (*rit) * n + c;
+        c = p / 10;
+        *rit = p % 10;
+    }
+    if (c > 0)
+        while (c > 0)
+        {
+            v.insert(v.begin(), c % 10);
+            c /= 10;
+        }
+}
+
+int main()
+{
+    vector<unsigned int> fact = {1};
+    for (unsigned int i = 1; i <= 100; i++)
+        multiply(fact, i);
+    unsigned int sum = 0;
+    for (unsigned int i = 0; i < fact.size(); i++)
+        sum += fact.at(i);
+    cout << sum << "\n";
+    return 0;
+}
