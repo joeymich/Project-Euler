dcd4051df76f698f3e7e17defe887abfa551eaf5
Jacob Immich <jacobimmich@gmail.com>
2022-04-02 01:39:22 -0400

    Optomize solution for problem #021

--- 021/021.cpp
+++ 021/021.cpp
@@ -1,18 +1,21 @@
 // Problem 21 -- Amicable numbers
 #include <iostream>
-#include <vector>
+#include <cmath>
 #include <map>
 using namespace std;
 
 unsigned int d(unsigned int a)
 {
-    vector<unsigned int> v;
-    for (unsigned int i = 1; i <= a / 2; i++)
-        if (a % i == 0)
-            v.push_back(i);
     unsigned int sum = 0;
-    for (unsigned int i = 0; i < v.size(); i++)
-        sum += v.at(i);
+    for (unsigned int i = 1; i <= sqrt(a); i++)
+        if (a % i == 0)
+        {
+            if (a / i == i)
+                sum += i;
+            else
+                sum += i + a / i;
+        }
+    sum -= a;
     return sum;
 }
 
