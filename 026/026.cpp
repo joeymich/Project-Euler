1c066cd1eec65048dbc217b96b977f432347cf6b
Jacob Immich <jacobimmich@gmail.com>
2022-04-04 16:47:46 -0400

    Add solution for problem #026

---
+++ 026/026.cpp
@@ -0,0 +1,45 @@
+// Problem 26 -- Reciprocal cycles
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+unsigned int consecutivePrimes(int a, int b);
+bool isPrime(int a);
+
+int main()
+{
+    int aMax, bMax;
+    unsigned int max = 0;
+    for (int a = -999; a < 1000; a++)
+        for (int b = -1000; b <= 1000; b++)
+        {
+            unsigned int temp = consecutivePrimes(a, b);
+            if (temp > max)
+            {
+                max = temp;
+                aMax = a;
+                bMax = b;
+            }
+        }
+    cout << aMax * bMax << "\n";
+    return 0;
+}
+
+unsigned int consecutivePrimes(int a, int b)
+{
+    int n = 0;
+    while (isPrime(n * n + a * n + b))
+        n++;
+    return n;
+}
+
+bool isPrime(int a)
+{
+    if (a <= 1 || a % 2 == 0)
+        return false;
+    else
+        for (unsigned int i = 3; i <= sqrt(a); i+=2)
+            if (a % i == 0)
+                return false;
+    return true;
+}
