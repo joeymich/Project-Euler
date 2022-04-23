de9b931de4e7a3458dd4847460d5c1a07d689b04
Jacob Immich <jacobimmich@gmail.com>
2022-04-23 16:25:00 -0400

    Add solution for problem #052

---
+++ 052/052.cpp
@@ -0,0 +1,35 @@
+// Problem 52 -- Permutated multiples
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool isPermutation(unsigned int n1, unsigned int n2)
+{
+    string s1 = to_string(n1);
+    string s2 = to_string(n2);
+    return is_permutation(s1.begin(), s1.end(), s2.begin());
+}
+
+int main()
+{
+    int num = 125875;
+    while(1)
+    {
+        for (int i = 2; i < 7; i++)
+        {
+            if (isPermutation(num, num*i))
+            {
+                if (i == 6)
+                    goto end;
+                continue;
+            }
+            else
+                break;
+        }
+        num++;
+    }
+    end:
+        cout << num << "\n";
+    return 0;
+}
