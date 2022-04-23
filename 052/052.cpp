c5951082754cf7a0d97fe8fa363f08ca9dc667e5
Jacob Immich <jacobimmich@gmail.com>
2022-04-23 16:27:29 -0400

    Adjust format

--- 052/052.cpp
+++ 052/052.cpp
@@ -4,12 +4,7 @@
 #include <algorithm>
 using namespace std;
 
-bool isPermutation(unsigned int n1, unsigned int n2)
-{
-    string s1 = to_string(n1);
-    string s2 = to_string(n2);
-    return is_permutation(s1.begin(), s1.end(), s2.begin());
-}
+bool isPermutation(unsigned int n1, unsigned int n2);
 
 int main()
 {
@@ -32,4 +27,11 @@ int main()
     end:
         cout << num << "\n";
     return 0;
+}
+
+bool isPermutation(unsigned int n1, unsigned int n2)
+{
+    string s1 = to_string(n1);
+    string s2 = to_string(n2);
+    return is_permutation(s1.begin(), s1.end(), s2.begin());
 }
